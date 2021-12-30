5ada190324f5989883e65b31f0d3da192943cb68
mausmani2494 <mausmani2494@gmail.com>
2021-12-30 07:48:52 -0600

    first commit

---
+++ scripts.js
@@ -0,0 +1,15 @@
+class Calculator{
+    constructor(preText, curText){
+        this.preText = preText
+        this.curText = curText
+    }
+
+}
+
+const numberButtons = document.querySelectorAll('[data-number]')
+const operationButtons = document.querySelectorAll('[data-operation]')
+const equalButton = document.querySelector('[data-equal]')
+const acButton = document.querySelector('[data-ac]')
+const delButton = document.querySelector('[data-del]')
+const preText = document.querySelector('[data-pre]')
+const curText = document.querySelector('[data-cur]')
